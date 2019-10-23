e1b623972e890e0991eb9a207dd41b407235ef91
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-10-22 21:26:58 -0500

    Clases para API, Retrofit para consumir y preliminar de diseño para prueba posterior en dispositivo movil

--- app/src/main/java/com/example/acompaaap/MainActivity.java
+++ app/src/main/java/com/example/acompaaap/MainActivity.java
@@ -3,8 +3,23 @@ package com.example.acompaaap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.acompaaap.model.Doctor;
+import com.example.acompaaap.remote.DoctorService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAddDoctor;
+    Button getBtnDoctorsList;
+    ListView listView;
+
+    DoctorService doctorService;
+    List<Doctor> list = new ArrayList<Doctor>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
