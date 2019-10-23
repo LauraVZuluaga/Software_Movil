3e74be7ce784209bc9e2ef372bde2afcd53902ea
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-10-22 21:42:28 -0500

    Asociación de los elementos de la interfaz

--- app/src/main/java/com/example/acompaaap/MainActivity.java
+++ app/src/main/java/com/example/acompaaap/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.acompaaap.model.Doctor;
+import com.example.acompaaap.remote.APIUtils;
 import com.example.acompaaap.remote.DoctorService;
 
 import java.util.ArrayList;
@@ -15,15 +16,21 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     Button btnAddDoctor;
-    Button getBtnDoctorsList;
+    Button btnGetDoctorsList;
     ListView listView;
 
     DoctorService doctorService;
     List<Doctor> list = new ArrayList<Doctor>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddDoctor = (Button) findViewById(R.id.btnAddDoctor);
+        btnGetDoctorsList = (Button) findViewById(R.id.btnGetDoctorsList);
+        listView = (ListView) findViewById(R.id.listView);
+        doctorService = APIUtils.getDoctorService();
     }
 }
